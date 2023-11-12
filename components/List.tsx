fb1465db10e5eda29c833999ed4c7a86cf51f1d0
Matt <m.lang@live.com>
2023-11-12 15:15:14 -0700

    remove unused styling

--- components/List.tsx
+++ components/List.tsx
@@ -35,16 +35,3 @@ export const List = (props: ListProps) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-      backgroundColor: '#f9c2ff',
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-    },
-  });
