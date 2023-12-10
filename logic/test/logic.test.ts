2987c33a8eba31181ec208dd7fed44947993fef5
Matt <m.lang@live.com>
2023-12-10 14:07:34 -0700

    test meet at take out

--- logic/test/logic.test.ts
+++ logic/test/logic.test.ts
@@ -1,20 +1,26 @@
 import { getTestScenario, zeroPad } from "./testScenarios";
-import { calculateMeetAtPutIn, isScenarioValid } from "../logic";
+import { calculateMeetAtTakeOut, isScenarioValid } from "../logic";
 import { ShuttleType } from "../Types";
 
 test("all scenarios up to x", async () => {
   //Rick & Morty scenario
   //[4, 0, 1, 0, 2, 0];
+  //4 people with 0 space
+  //0 people with 1 space
+  //1 people with 2 space
+  //0 people with 3 space
+  //2 people with 4 space
+  //0 pepole with 5 space
 
   const max = 999999;
-  const shuttleType = ShuttleType.MEET_AT_PUT_IN;
+  const shuttleType = ShuttleType.MEET_AT_TAKE_OUT;
 
   for (let number = 0; number < max; number++) {
     const numArray = zeroPad(number).split("").map(Number);
     const scenario = getTestScenario(shuttleType, numArray);
     const { people, vehicles } = scenario;
     if (isScenarioValid(people, vehicles, shuttleType)) {
-      expect(calculateMeetAtPutIn(people, vehicles)).not.toThrow;
+      expect(calculateMeetAtTakeOut(people, vehicles)).not.toThrow;
     }
   }
 });
